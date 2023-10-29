f89d39a8c073af5c70b6a80d1db7dc89ca4738e8
Antonio Guiotto <antonio@Antonios-MacBook-Pro.local>
2023-10-29 11:54:27 +0100

    gitignore

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ router.get("/", function (req, res, next) {
   res.json({ title: "server running" });
 });
 
-const options = ["-vf", "pad=ceil(iw/2)*2:ceil(ih/2)*2"];
+const options = [
+  "-vf",
+  "scale=iw:ih:force_original_aspect_ratio=1,pad=ceil(iw/2)*2:ceil(ih/2)*2:(ow-iw)/2:(oh-ih)/2",
+];
 
 app.post("/api/video-upload", upload.single("video"), (req, res, next) => {
   const videoPath = req.file.path;
@@ -28,12 +31,11 @@ app.post("/api/video-upload", upload.single("video"), (req, res, next) => {
     let ffmpegOutcome = "";
     ffmpeg(videoPath)
       .outputOptions(options)
-      .saveToFile("output.mp4", (error, info) => {
-        if (info) {
-          ffmpegOutcome = info;
-        }
+      .saveToFile("uploaded_video.mp4", (error, info) => {
         if (error) {
-          ffmpegOutcome = error;
+          res.status(500).send(error.message);
+        } else {
+          res.status(200).send({ message: "Video processed successfully", info });
         }
       });
     res.status(200).send({ ffmpegOutcome });
