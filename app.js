fa4b2d31c4b2661893097f119df3f653e1258c03
Antonio Guiotto <antonio@Antonios-MacBook-Pro.local>
2023-10-29 13:14:31 +0100

    squaring up videos and saving them on the server dir

--- app.js
+++ app.js
@@ -7,38 +7,47 @@ const ffmpeg = require("fluent-ffmpeg");
 const upload = multer({ dest: "uploads/" });
 
 const app = express();
-const router = express.Router();
 
-app.use(router);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-router.get("/", function (req, res, next) {
-  res.json({ title: "server running" });
-});
-
-const options = [
-  "-vf",
-  "scale=iw:ih:force_original_aspect_ratio=1,pad=ceil(iw/2)*2:ceil(ih/2)*2:(ow-iw)/2:(oh-ih)/2",
-];
-
 app.post("/api/video-upload", upload.single("video"), (req, res, next) => {
   const videoPath = req.file.path;
+  const bgColor = req.body.bgcolor;
   try {
-    let ffmpegOutcome = "";
+    ffmpeg.setFfmpegPath("/usr/local/bin/ffmpeg");
+    ffmpeg.setFfprobePath("/usr/local/bin/ffprobe");
     ffmpeg(videoPath)
-      .outputOptions(options)
-      .saveToFile("uploaded_video.mp4", (error, info) => {
-        if (error) {
-          res.status(500).send(error.message);
-        } else {
-          res.status(200).send({ message: "Video processed successfully", info });
-        }
-      });
-    res.status(200).send({ ffmpegOutcome });
+      .addOptions([
+        `-filter_complex`,
+        `scale='iw*sar':'ih',pad='max(iw\\,ih)':'max(iw\\,ih)':'(ow-iw)/2':'(oh-ih)/2':${
+          bgColor || "black"
+        }`,
+      ])
+      .on("start", function (commandLine) {
+        console.log("Spawned FFmpeg with command: " + commandLine);
+      })
+      .on("error", function (err, stdout, stderr) {
+        console.error("Error:", err.message);
+        console.error("ffmpeg stdout:", stdout);
+        console.error("ffmpeg stderr:", stderr);
+      })
+      .on("end", function (output) {
+        console.log("Video processed:", output);
+        res
+          .status(200)
+          .send({ message: "Video processed successfully", output });
+      })
+      .on("stderr", function (stderrLine) {
+        console.log("Stderr output: " + stderrLine);
+      })
+      .on("stdout", function (stdoutLine) {
+        console.log("Stdout output: " + stdoutLine);
+      })
+      .saveToFile("uploaded_video.mp4");
   } catch (error) {
     res.status(500).send(error.message);
   }
